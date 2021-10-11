04aee5820d1e3c7b1f7bc207c2a00610d9df9a26
NguyenTrung210402 <91875395+NguyenTrung210402@users.noreply.github.com>
2021-10-11 14:24:52 +0700

    upp

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -19,7 +19,25 @@ int main(int argc, char *argv[]) {
 	float distance = atof(argv[1]);
 	int result;
 	//Your codes here
-	
+{
+    float TotalKm;
+    float Cost = 0;
+    printf("\n Input: ");
+    scanf("%f", &TotalKm);
+    if(TotalKm <= 2)
+    {
+        Cost = TotalKm * 15000;
+    }
+    else if(TotalKm <= 30  && TotalKm > 2 );
+    {
+        Cost = (TotalKm - 2)*2000/0.25 + 2*15000;
+    }
+	if(TotalKm > 30);
+    {
+        Cost = 1*15000 + (30-2)/0.25*2000 + (TotalKm - 30)*5000;
+    }
+    printf(" Output: %0.3f ", Cost);
+}
 	printf("%d", result);
 	return 0;
 }
